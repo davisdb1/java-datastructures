590a6f849ffbf8d2d250c6feb03601288a0e3c50
Craig Nishina <craig.nishina@gmail.com>
2014-06-09 23:22:54 -0700

    added some comments around the recursion

--- src/main/java/com/cnc/algorithms/StringPermutations.java
+++ src/main/java/com/cnc/algorithms/StringPermutations.java
@@ -16,13 +16,16 @@ public class StringPermutations {
 			return res;
 		}
 		
-	
+		// recursively call GetPermutations to start at the last char.
+		// unwinding this method will introduce the chars one at a time
 		List<String> permutations = GetPermutations(word.substring(1));
+		
+		// rotate the location of the perm char in all positions over the 
+		// permutation string. do this for all permutations in the list
 		int permLength = permutations.get(0).length();
-		char permChar = word.charAt(0);
-		List<String> result = new ArrayList<String>();
-		
-		
+      char permChar = word.charAt(0);
+      List<String> result = new ArrayList<String>();
+      
 		for(String permutation : permutations){
 			for(int i = 0; i <= permLength; i++){
 					result.add(permutation.substring(0,i) + permChar + permutation.substring(i));
