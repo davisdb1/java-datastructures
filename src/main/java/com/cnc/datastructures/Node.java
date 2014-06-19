58b911dfc8a45af63937629c860e7e987c92f7fd
Craig Nishina <craig.nishina@gmail.com>
2014-06-18 23:30:51 -0700

    fix node to have an alias for prev and next nodes as left and right for a tree

--- src/main/java/com/cnc/datastructures/Node.java
+++ src/main/java/com/cnc/datastructures/Node.java
@@ -2,8 +2,8 @@ package com.cnc.datastructures;
 
 public class Node<T> {
 	private T data;
-	private Node<T> next;
-	private Node<T> prev;
+	private Node<T> next;  // or right
+	private Node<T> prev;  // or left
 	
 	
 	public T getData() {
@@ -12,11 +12,23 @@ public class Node<T> {
 	public void setData(T data) {
 		this.data = data;
 	}
+	public Node<T> getRight() {
+	   return next;
+	}
+	public void setRight(Node<T> right) {
+      this.next = right;
+   }
 	public Node<T> getNext() {
 		return next;
 	}
 	public void setNext(Node<T> next) {
 		this.next = next;
+	}
+	public Node<T> getLeft() {
+	   return prev;
+	}
+	public void setLeft(Node<T> left) {
+	   this.prev = left;
 	}
 	public Node<T> getPrev() {
 		return prev;
