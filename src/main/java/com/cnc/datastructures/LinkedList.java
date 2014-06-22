cda86131a33a1d7b787ebf7c76b064042d0f4997
Craig Nishina <craig.nishina@gmail.com>
2014-06-21 18:57:41 -0700

    adding a remove method and testing

--- src/main/java/com/cnc/datastructures/LinkedList.java
+++ src/main/java/com/cnc/datastructures/LinkedList.java
@@ -15,15 +15,52 @@ public class LinkedList<T> {
 		tail = node;
 	}
 	
+	public boolean remove(int index) {
+	   // no nodes
+	   if (head == null) {
+	      return false;
+	   }
+	   // for head node
+	   if (index == 0) {
+	      Node<T> tmp = head;
+	      head = tmp.getNext();
+	      tmp = null;
+	      return true;
+	   }
+	   
+	   // for the rest of the nodes
+	   Node<T> prev = head;
+      Node<T> curr = head.getNext(); 
+	   int currIndex = 1;
+	   while (curr != null) {
+	      // remove the index
+	      if (currIndex == index) {
+	         prev.setNext(curr.getNext());
+	         // if at the tail, set tail to previous
+	         if (curr.getNext() == null)
+	            tail = prev;
+	         curr = null;
+	         return true;
+	      }
+	      // not at the index, move forward
+	      else {
+	         currIndex++;
+	         prev = curr;
+	         curr = curr.getNext();
+	      }
+	   }
+	   return false;
+	}
+	
 	public T getAtIndex(int index){
-		Node<T> current = head;
-		int currentIndex = 0;
-		while(current != null){
-			if(currentIndex == index){
-				return current.getData();
+		Node<T> curr = head;
+		int currIndex = 0;
+		while(curr != null){
+			if(currIndex == index){
+				return curr.getData();
 			}else{
-				current = current.getNext();
-				currentIndex++;
+			   curr = curr.getNext();
+			   currIndex++;
 			}
 		}
 		return null;
@@ -31,16 +68,16 @@ public class LinkedList<T> {
 	
 	public void reverse(){
 		if(head != null){
-			Node<T> current = head;
+			Node<T> curr = head;
 			Node<T> prev = null;
 			Node<T> tmp = tail;
 			tail = head;
 			head = tmp;
-			while(current != null){
-				tmp = current.getNext();
-				current.setNext(prev);
-				prev = current;
-				current = tmp;
+			while(curr != null){
+				tmp = curr.getNext();
+				curr.setNext(prev);
+				prev = curr;
+				curr = tmp;
 			}
 		}
 	}
