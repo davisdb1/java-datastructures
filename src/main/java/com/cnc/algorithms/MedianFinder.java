01b2855534195b1e37e2bcb0d1a437c5936cd1a6
WhiteBoardDev <evan.tyler.harris@gmail.com>
2014-10-02 09:23:09 -0700

    Fixing broken medianFinder and adding String Reversal

--- src/main/java/com/cnc/algorithms/MedianFinder.java
+++ src/main/java/com/cnc/algorithms/MedianFinder.java
@@ -14,37 +14,52 @@ public class MedianFinder {
 	public void addNumber(int value){
 		Node<Integer> node = new Node<Integer>();
 		node.setData(value);
-
+		numberOfInserts++;
+		if(root == null) {
+		  root = node;
+		  return;
+		}
+		
 		Node<Integer> current = root;
 		Node<Integer> prev = null;
+	
 		while(current != null && current.getData() < value){
 			prev = current;
 			current = current.getNext();
 		}
-		if(prev == null){
-			root = node;
-		}else{
-			if(current == null){
-				prev.setNext(node);
-			}else{
-				node.setNext(current);
-				prev.setNext(node);
-			}
+		
+		//insert at the beginnning
+		if(prev == null) {
+		  node.setNext(current);
+		  root = node;
+		}else {
+		  //insert at middle/end
+		  node.setNext(current);
+		  prev.setNext( node );
 		}
-
-		numberOfInserts++;
+		
 	}
 
 	public int getMedian(){
 		Node<Integer> current = root;
-		for(int i = 1; i  < numberOfInserts / 2 ; i++){
+		Node<Integer> prev = root;
+		
+		for(int i = 1; i  <= numberOfInserts / 2 ; i++){
+		    prev = current;
 			current = current.getNext();
 		}
-		if(current != null){
-			return current.getData();
-		}else{
-			return -1;
+		if(current != null) {
+    		if(numberOfInserts % 2 == 1) {
+    		  return current.getData();
+    		}else {
+    		  return (prev.getData() + current.getData()) / 2;
+    		}
 		}
+		else
+		{
+          return -1;
+		}
+		
 		
 	}
 }
--- src/test/java/com/cnc/algorithms/MedianFinderTest.java
+++ src/test/java/com/cnc/algorithms/MedianFinderTest.java
@@ -33,6 +33,44 @@ public class MedianFinderTest {
 	}
 	
 	@Test
+    public void medainSequential(){
+        MedianFinder med = new MedianFinder();
+        med.addNumber(1);
+        med.addNumber(2);
+        med.addNumber(3);
+        med.addNumber(4);
+        med.addNumber(5);
+        int result = med.getMedian();
+        assertEquals(result, 3);
+    }
+	
+	   @Test
+	    public void medainNonSequential(){
+	        MedianFinder med = new MedianFinder();
+	        med.addNumber(1);
+	        med.addNumber(5);
+	        med.addNumber(3);
+	        med.addNumber(4);
+	        med.addNumber(2);
+	        int result = med.getMedian();
+	        assertEquals(result, 3);
+	    }
+	    
+	   
+	   @Test
+       public void medainLargeNumberDifference(){
+           MedianFinder med = new MedianFinder();
+           med.addNumber(100);
+           med.addNumber(-100);
+           med.addNumber(0);
+           med.addNumber(30);
+          
+           int result = med.getMedian();
+           assertEquals(result, 15);
+       }
+       
+	
+	@Test
 	public void  multipleOfSameValue(){
 		MedianFinder med = new MedianFinder();
 		med.addNumber(6);
